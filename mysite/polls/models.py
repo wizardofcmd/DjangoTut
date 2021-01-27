90daf57d68317925920453261191b3f5994d5229
wizardofcmd <sorinnechiforora@gmail.com>
2021-01-27 15:12:16 +0000

    First pull request

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -8,3 +8,5 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+# Blank comment here
